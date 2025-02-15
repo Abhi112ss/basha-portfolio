880e3c63831d2452d2469ddf336f026a1765af69
Abhi112ss <134950195+Abhi112ss@users.noreply.github.com>
2025-02-15 20:27:24 +0530

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -17,8 +17,8 @@ const Contact = () => {
             </div>
 
             <div className={"bg-white/10 p-6 rounded-xl max-w-[550px]"}>
-                <h2 className={"text-5xl font-bold text-orange-400 mb-4"}>Let's Connect</h2>
-                <p className={"text-white/70 mb-6"}>Send a Message and let's Schedule a Call!</p>
+                <h2 className={"text-5xl font-bold text-orange-400 mb-4"}>Let&apos;s Connect</h2>
+                <p className={"text-white/70 mb-6"}>Send a Message and let&apos;s Schedule a Call!</p>
                 <form className={"space-y-4"} action={"https://getform.io/f/bqokoevb"} method={"POST"}>
                     <div className={"grid md:grid-cols-2 gap-4"}>
                         <input type={"text"} name={"name"} className={"bg-black/70 rounded-xl p-3 focus:outline-none focus:ring-2 focus:ring-orange-400"} placeholder={"First Name"}/>
