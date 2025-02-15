a4b2f2c4c04d031d245cd1d017d3d879fb3e6a09
Abhi112ss <134950195+Abhi112ss@users.noreply.github.com>
2025-02-15 20:35:14 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono ,Poppins} from "next/font/google";
+import {Poppins} from "next/font/google";
 import "./globals.css";
 import React from "react";
-import {color} from "framer-motion";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
 const poppins = Poppins({
     weight: ["100","200","300","400","500","600","700","800","900"],
     subsets:["latin"]
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
